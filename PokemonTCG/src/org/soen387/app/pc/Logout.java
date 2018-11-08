e140723bbdcd89cb1c6e81ce6f353658e610a5e9
BrandonAmyot <amyot.brandon@hotmail.com>
2018-11-07 20:16:47 -0500

    logout added

---
+++ PokemonTCG/src/org/soen387/app/pc/Logout.java
@@ -0,0 +1,75 @@
+package org.soen387.app.pc;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/Logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		try {
+			DBCon.myCon.set(DriverManager.getConnection("jdbc:mysql://localhost/amyot_brandon?"
+					+"user=amyot_brandon&password=mberfrab&characterEncoding=UTF-8&useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true"));
+			
+			processRequest(request, response);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			Connection con = DBCon.myCon.get();
+			DBCon.myCon.remove();
+			try {con.close();} catch(Exception e) {}
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+	
+	public void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			long id = (long)request.getSession(true).getAttribute("userid");
+			UserRDG u = UserRDG.find(id);
+			
+			request.getSession(true).invalidate();
+			request.setAttribute("message", "User '" + u.getUsername() + "' has been successfully logged out.");
+			request.getRequestDispatcher("WEB-INF/jsp/success.jsp").forward(request, response);								
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
